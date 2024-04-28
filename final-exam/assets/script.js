3621dd9a9669dc76514234d7f37c421c7078863e
Nathan Reeves <nreeves.em@gmail.com>
2024-04-28 14:34:34 -0500

    changing project

--- final-exam/assets/script.js
+++ final-exam/assets/script.js
@@ -1,63 +1,52 @@
-async function typewriter(text, elementId, waitAfter, collapse_after=true) {
-    var n = 0,
-        isTag = false
-    addText = "";
-    const el = document.getElementById(elementId);
-
-    const wait = () => new Promise(r => setTimeout(r, waitAfter));
-    const nowait = () => new Promise(r => r());
-
-    const render = () => el.innerHTML = (text.slice(0, n + 1) + addText);
-
-    const cursor = document.createElement('span');
-    cursor.id = "blink";
-
-    el.style.setProperty("margin-right", "-10px");
-    el.style.setProperty("--cursor-visibility", "visible");
-    while (n < text.length) {
-        if (text.charAt(n + 1) === "<") isTag = true;
-        if (text.charAt(n + 1) === ">") isTag = false;
-
-        if (isTag) {
-            n++;
-            continue;
-        }
-
-        requestAnimationFrame(render);
-
-        if (waitAfter === 0) {
-            await nowait();
-        } else {
-            await wait();
-        }
-
-        n++;
+var MovesCount=0
+function randomPos(){
+    var arr = [];
+    while(arr.length < 9){
+        var r = ((Math.floor(Math.random() * 3)+1).toString())+((Math.floor(Math.random() * 3)+1).toString());
+        if(arr.indexOf(r) === -1) arr.push(r);
     }
-    if (collapse_after) {
-        el.style.setProperty("--cursor-visibility", "collapse");
+    return arr
+}
+
+var RandomPos=randomPos()
+
+for(let i =0;i<document.getElementsByClassName("tile").length;i++){
+    document.getElementsByClassName("tile")[i].style.gridArea=RandomPos[i][0]+"/"+RandomPos[i][1]
+}
+function MoveMe(tile){
+    var EmptyTile=document.querySelector(".emtile")
+    var Possibilties=[
+        parseInt(RandomPos[tile][0])+1==parseInt(RandomPos[8][0])&&parseInt(RandomPos[tile][1])==parseInt(RandomPos[8][1]),
+        parseInt(RandomPos[tile][0])-1==parseInt(RandomPos[8][0])&&parseInt(RandomPos[tile][1])==parseInt(RandomPos[8][1]),
+        parseInt(RandomPos[tile][1])+1==parseInt(RandomPos[8][1])&&parseInt(RandomPos[tile][0])==parseInt(RandomPos[8][0]),
+        parseInt(RandomPos[tile][1])-1==parseInt(RandomPos[8][1])&&parseInt(RandomPos[tile][0])==parseInt(RandomPos[8][0]),
+    ]
+if(Possibilties[0]||Possibilties[1]||Possibilties[2]||Possibilties[3]){
+    MovesCount++;
+    EmptyTile.style.gridArea=RandomPos[tile][0]+"/"+RandomPos[tile][1];
+    document.querySelectorAll(".tile")[tile].style.gridArea=RandomPos[8][0]+"/"+RandomPos[8][1];
+
+    var CurrentTile=RandomPos[tile]
+    RandomPos[tile]=RandomPos[8]
+    RandomPos[8]=CurrentTile;
+    NeededPos=["11","12","13","21","22","23","31","32","33"]
+    if(RandomPos.join(".")==NeededPos.join(".")){
+        console.log("Game Beated");
+        document.querySelector(".blscreen").style.display='flex'
+        document.querySelector(".MovesCount").innerHTML=MovesCount;
+        var stars=0;
+        if(MovesCount<100){
+            stars=3
+        }else if(MovesCount<200){
+            stars=2
+        }else if(MovesCount<300){
+            stars=1
+        }else{
+            stars=0
+        }
+        for(let i=0;i<2;i++){
+            document.getElementsByTagName("path").style.fill="yellow"
+        }
     }
 }
-
-function parseDelay(d) {
-    const parsed = parseInt(d, 10);
-    if (isNaN(parsed)) return 0;
-    return parsed;
 }
-
-const ps1Delay = parseDelay("15"),
-    stdoutDelay = parseDelay("15"),
-    commandDelay = parseDelay("15");
-const typeeffetct = async () => {
-    await typewriter("\u003cstrong\u003e\u003cspan id=\u0027user\u0027\u003e@nreeves.em\u003c\/span\u003e\u003cspan id=\u0027terminal\u0027\u003e:\u003c\/span\u003e\u003cspan id=\u0027dir\u0027\u003e~\/\u003c\/span\u003e\u003c\/strong\u003e\u003cspan id=\u0027terminal\u0027\u003e$\u003c\/span\u003e", "ps1_01", ps1Delay); 
-    await typewriter("\u003cspan id=\u0027terminal\u0027\u003ecd About\u003c\/span\u003e", "cd", commandDelay);
-    await typewriter("\u003cstrong\u003e\u003cspan id=\u0027user\u0027\u003e@nreeves.em\u003c\/span\u003e\u003cspan id=\u0027terminal\u0027\u003e:\u003c\/span\u003e\u003cspan id=\u0027dir\u0027\u003e~\/About\u003c\/span\u003e\u003c\/strong\u003e\u003cspan id=\u0027terminal\u0027\u003e$ more About.txt\u003c\/span\u003e", "ps1_02", ps1Delay); 
-   
-    await typewriter("\u003cspan id=\u0027terminal\u0027\u003e\u003cbr\u003e\       My name is Nathan,\u003cbr\u003e\n          \u003cbr\u003e\n\u003cbr\u003e\n  I'm a Software Engineer\u003c\/\u003e \u003c\/p\u003e\n\u003c\/span\u003e", "std_out_01", stdoutDelay);
-    await typewriter("\u003cstrong\u003e\u003cspan id=\u0027user\u0027\u003e@nreeves.em\u003c\/span\u003e\u003cspan id=\u0027terminal\u0027\u003e:\u003c\/span\u003e\u003cspan id=\u0027dir\u0027\u003e~\/About\u003c\/span\u003e\u003c\/strong\u003e\u003cspan id=\u0027terminal\u0027\u003e$\u003c\/span\u003e", "ps1_03", ps1Delay); 
-    await typewriter("\u003cspan id=\u0027terminal\u0027\u003els .\/Links\/\u003c\/span\u003e", "ls", commandDelay);
-    await typewriter("\u003cspan id=\u0027terminal\u0027\u003e\u003cspan id=\u0027terminal\u0027\u003e\u003cspan id=\u0027terminal\u0027\u003e\u003cstrong\u003e\u003cspan id=\u0027dir\u0027\u003e.\/Links\/\u003c\/span\u003e\u003c\/strong\u003e\u003cbr\u003e├── \u003ca href=\u0027http:\/\www.instagram.com/nreeves.em\u0027\u003eInstagram\u003c\/a\u003e\u003c\/span\u003e\u003cbr\u003e├── \u003ca href=\u0027https://github.com/nreeves\u0027\u003eGithub\u003c\/a\u003e\u003c\/span\u003e\u003cbr\u003e└── \u003ca href=\u0027www.linkedin.com/in/reevesnate\u0027\u003eLinkedIn\u003c\/a\u003e\u003c\/span\u003e", "std_out_02", stdoutDelay);
-    await typewriter("\u003cstrong\u003e\u003cspan id=\u0027user\u0027\u003e@nreeves.em\u003c\/span\u003e\u003cspan id=\u0027terminal\u0027\u003e:\u003c\/span\u003e\u003cspan id=\u0027dir\u0027\u003e~\/Links\u003c\/span\u003e\u003c\/strong\u003e\u003cspan id=\u0027terminal\u0027\u003e$\u003c\/span\u003e ", "ps1_04", ps1Delay, false);
-    return;
-}
-
-typeeffetct()
